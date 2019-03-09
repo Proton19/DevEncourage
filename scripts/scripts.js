213c1b98ecd9a0ae4d9324bff6274f044455a7aa
Sandrico Provo <Sprovo@outlook.com>
2019-03-09 08:48:33 -0400

    Uploading files

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+const happyText = document.querySelector('.name--text');
+const firstName = document.querySelector('.name--firstname');
+const lastName = document.querySelector('.name--lastname');
+
+const submitBtn = document
+  .querySelector('.name--submit')
+  .addEventListener('click', () => {
+    const happyTextArr = [
+      `Hey there, ${firstName.value} ${
+        lastName.value
+      }. You rock, don't ever change 😄`,
+      `${firstName.value} ${lastName.value}, you so got this 💪`,
+      `Don't quit ${firstName.value}, you can totally do this.`,
+      `Hooraaaaaaaay for ${firstName.value} 😃`,
+      `Be Happy ${firstName.value}; do what you love 🤩`,
+    ];
+    happyText.textContent =
+      happyTextArr[Math.floor(Math.random() * happyTextArr.length)];
+  });
